ff1eef453b637fa2f8e24a9e89978cac8b73ca10
Karol Stypa <bury09@gmail.com>
2017-10-27 02:19:39 +0200

    lab3.zad5Mail sending test added

--- src/lab3/zad5Mail/EmailMain.java
+++ src/lab3/zad5Mail/EmailMain.java
@@ -1,10 +1,16 @@
 package lab3.zad5Mail;
 
 public class EmailMain {
-//    EmailMessage wiadomosc = EmailMessage.builder();
-//            .addFrom("sbobek@agh.edu.pl");
-//            .addTo("student@agh.edu.pl")
-//            .addSubject("Mail testowy")
-//            .addcontent("Brak tresci")
-//            .build();
+    public static void main(String[] args) {
+        System.out.println("Wysyłanie wiadomości testowej");
+        EmailMessage wiadomosc = EmailMessage.builder()
+                .addFrom("informatyka.4b@gmail.com")
+                .addTo("bury09@gmail.com")
+                .addTo("kstypa@student.agh.edu.pl")
+                .addSubject("Mail testowy")
+                .addContent("Brak tresci")
+                .build();
+
+        wiadomosc.send();
+    }
 }
--- src/lab3/zad5Mail/EmailMessage.java
+++ src/lab3/zad5Mail/EmailMessage.java
@@ -1,6 +1,12 @@
 package lab3.zad5Mail;
 
 import java.util.LinkedList;
+import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 public class EmailMessage {
 
@@ -34,20 +40,34 @@ public class EmailMessage {
         private String subject;
         private String content;
 
-        public void addFrom(String from) {
-            this.from = from;
+        public static final Pattern EMAIL_REGEX =
+                Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+
+        public static boolean CheckEmail(String email) {
+            Matcher matcher = EMAIL_REGEX.matcher(email);
+            return matcher.find();
         }
 
-        public void addTo(String to) {
-            this.to.add(to);
+        public Builder addFrom(String from) {
+            if(CheckEmail(from))
+                this.from = from;
+            return this;
         }
 
-        public void addSubject(String subject) {
+        public Builder addTo(String to) {
+            if(CheckEmail(to))
+                this.to.add(to);
+            return this;
+        }
+
+        public Builder addSubject(String subject) {
             this.subject = subject;
+            return this;
         }
 
-        public void addContent(String content) {
+        public Builder addContent(String content) {
             this.content = content;
+            return this;
         }
 
         public EmailMessage build() {
@@ -58,5 +78,47 @@ public class EmailMessage {
 
     public static Builder builder() {
         return new EmailMessage.Builder();
+    }
+
+    public void send() {
+        final String username = "informatyka.4b@gmail.com";
+        final String password = "informatyka123";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", "587");
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+//        Session session = Session.getInstance(props, null);
+        session.setDebug(true);
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            for(String t : to)
+                message.addRecipient(Message.RecipientType.TO, new InternetAddress(t));
+            message.setSubject(subject);
+            message.setText(content);
+
+//            Transport.send(message, username, password);
+            Transport transport = session.getTransport("smtp");
+            transport.connect("smtp.gmail.com", username, password);
+            System.out.println("Transport: "+transport.toString());
+            transport.sendMessage(message, message.getAllRecipients());
+
+            System.out.println("Wysłano wiadomość");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e) {}
     }
 }
